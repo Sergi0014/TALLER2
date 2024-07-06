f1fa764803c4a59e5270c8e376497a549d04d25f
Sergi0024 <162249445+Sergi0014@users.noreply.github.com>
2024-07-06 17:38:56 -0500

    Add files via upload

---
+++ P2/js/script.js
@@ -0,0 +1,9 @@
+function showChart() {
+    // Muestra el elemento que contiene los porcentajes
+    document.getElementById('cityPercentages').style.display = 'block';
+}
+
+function hideChart() {
+    // Oculta el elemento que contiene los porcentajes
+    document.getElementById('cityPercentages').style.display = 'none'  
+}
